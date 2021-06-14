babf1cabf6a3e1260f2763bc1afe2cd212c2bd05
huebert04 <hmf1494@yahoo.com>
2021-06-14 13:16:35 +0200

    from bootstrap to vuetify (material)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/products">Products</router-link> |
-  </div>
+  <v-card>
+    <v-app-bar dark prominent>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary >
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+        <v-list-item v-for="item in listItems" :key="item.title" :to="item.path">
+          <v-list-item-action><v-icon>{{ item.icon }}</v-icon></v-list-item-action>
+          <v-list-item-content>{{item.title}}</v-list-item-content>
+        </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main> 
+  </v-card>
+  
 </template>
 
 <script>
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    props: ['drawer', 'group'],
+    data() {
+      return {
+        name: 'Vue SPA Nettbutikk',
+        sidebar: false,
+        listItems: [
+          {title: 'Home', path: '/', icon: 'home'},
+          {title: 'Products', path: '/products', icon: 'basket'}
+        ]
+      }
+    },
+    watch: {
+      drawer(value) {
+        return value;
+      }
+    }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
 import ProductDetail from '../components/ProductDetail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
   {
     path: '/products',
     name: 'Products',
