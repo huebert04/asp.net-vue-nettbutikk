34fbf86c0fc4cd5ec6f2e714f95ba62d31b9623e
huebert04 <hmf1494@yahoo.com>
2021-06-19 15:29:00 +0200

    added get all products and create a product function for api

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,7 +1,23 @@
 //let products = [];
 const db = require("../config/database");
 
+//get all products in db
 exports.getProducts = async (req, res) => {
     const response = await db.query('SELECT * FROM products');
     res.status(200).send(response.rows);
 };
+
+//create a product
+exports.createProduct = async (req, res) => {
+    const { productname, price, category, description, image} = req.body;
+    const { rows } = await db.query('INSERT INTO products (productname, price, category, description, image) VALUES ($1, $2, $3, $4, $5)',
+    [productname, price, category, description, image ]);
+
+    console.log(rows);
+    res.status(201).send({
+        message: "Product added!",
+        body: {
+            product: {productname, price, category, description, image}
+        }
+    })
+}
--- api/routes/products.routes.js
+++ api/routes/products.routes.js
@@ -1,7 +1,8 @@
-const { getProducts } = require('../controllers/products.controller');
+const { getProducts, createProduct } = require('../controllers/products.controller');
 
 const router = require('express-promise-router')();
 
 router.get('/products', getProducts);
+router.post('/products', createProduct);
 
 module.exports = router;
